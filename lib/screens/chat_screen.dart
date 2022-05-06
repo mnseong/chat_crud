873da9f088fb7a84207d4cfd7e9f34446c17121a
mnseong <mskweon@icloud.com>
2022-05-07 03:43:24 +0900

    chatting message 보내기 기능

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,9 @@
+import 'package:chat_crud/chat_storage/chat/new_message.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../chat_storage/chat/message.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -35,7 +38,13 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat screen'),
+        title: const Text(
+            'Chat screen',
+          style: TextStyle(
+            color: Colors.orange
+          ),
+        ),
+        backgroundColor: Colors.amberAccent,
         actions: [
           IconButton(
             onPressed: () {
@@ -48,35 +57,15 @@ class _ChatScreenState extends State<ChatScreen> {
           )
         ],
       ),
-      body: StreamBuilder(
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-
-          return ListView.builder(
-              itemCount: docs.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    docs[index]['text'],
-                    style: const TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.orange
-                    ),
-                  ),
-                );
-              }
-          );
-        },
-        stream: FirebaseFirestore.instance.collection(
-            'chats/rntWEpsnVTohgZRE8AId/message'
-        ).snapshots(),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+                child: Messages()
+            ),
+            NewMessage()
+          ],
+        ),
       )
     );
   }
